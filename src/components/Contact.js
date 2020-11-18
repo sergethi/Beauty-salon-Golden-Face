b63872a8dd9b48867a0eb48953813281a05c642d
serge aristide <nsergearistide@gmail.com>
2020-11-18 02:32:30 -0500

    added content in contact page and changed form looks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,27 +7,27 @@ import axios from 'axios'
 
 
 
-const styles = (theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-  area:{width:"30ch"},
-  name: {width:"17ch"},
-  email:{width:"17ch"},
+// const styles = (theme) => ({
+//   root: {
+//     '& > *': {
+//       margin: theme.spacing(1),
+//     },
+//   },
+//   area:{width:"30ch"},
+//   name: {width:"17ch"},
+//   email:{width:"17ch"},
 
 
-  notchedOutline: {
-    borderWidth: '1px',
-    borderColor: 'white !important'
-  },
-  cssLabel: {
-    color : 'white'
-  },
+//   notchedOutline: {
+//     borderWidth: '1px',
+//     borderColor: 'white !important'
+//   },
+//   cssLabel: {
+//     color : 'white'
+//   },
  
 
-});
+// });
 
 
 class Contact extends Component {
@@ -105,7 +105,52 @@ class Contact extends Component {
     const { classes } = this.props;
     return (
       <section className="contact-main">
-      <h1>Contact Us </h1>
+            <div class="hero">
+              <h1>Contact Us</h1>
+            </div>
+          <div className="contact-flex-container">
+            <div className="left-container">
+
+              <div className="form-address">
+              <img src="/images/addressIcon.png" width="24" height="24" alt="Address"/><h2>Address</h2><br />
+              <p>8381 Longbranch St.Hollis, NY 11423</p>
+              </div>
+
+              <div className="form-call">
+              <img src="/images/phone-call.png" width="24" height="24" alt="Phone"/><h2>Call Us</h2><br />
+              <p>(887) 201-1375</p>
+              </div>
+
+              <div className="form-envelop">
+              <img src="/images/envelope.png" width="24" height="24" alt="envelope"/><h2>General Support</h2><br />
+              <p>mjewell@outlook.com</p>
+              </div>
+
+            </div>
+            <form onSubmit={this.handleSubmit}>
+            <p>Fill out this form, and one of our coffee specialists will get in touch with you to figure out which coffees are best suited to your tastes. This personalized attention ensures you’ll get the most our of your Hipstirred membership.</p>
+              <label for="fullName">Full Name</label>
+              <input  className="fullNameField" type="text" value = {this.state.fullname} onChange = {this.handleFullName}/>
+              <label for="email">Email</label>
+              <input  className="emailField" type="text" value = {this.state.email} onChange = {this.handleEmail}/>
+              <label for="message">Message</label>
+              <textarea rows={10}  className="messageField"  type="text" value = {this.state.message} onChange = {this.handleMessage}/><br/>
+              <button type="submit"  className="contact">Let's talk</button>
+              
+          </form>
+
+          </div>
+          
+
+      </section>
+    )
+  }
+}
+export default Contact;
+
+{/* export default withStyles(styles)(Contact); */}
+
+      {/* <h1>Contact Us </h1>
      <form  onSubmit={this.handleSubmit}  className={classes.root} noValidate autoComplete="off" 
       
      >
@@ -177,89 +222,6 @@ class Contact extends Component {
      >
        Send
      </Button>
-   </form>
-   </section>
-    )
-  }
-}
-export default withStyles(styles)(Contact);
+   </form> */}
+  
 
-
-
-// function Contact() {
-//   const classes = useStyles();
-
-//   return (
-//     <section className="contact-main">
-//        <h1>Contact Us </h1>
-//       <form  className={classes.root} noValidate autoComplete="off" 
-       
-//       >
-     
-//       <TextField className={classes.name} id="outlined-basic" label="Name" variant="outlined" 
-//        InputProps={{
-//         classes: {
-//           root: classes.cssOutlinedInput,
-//           notchedOutline: classes.notchedOutline,
-//         },
-//         inputMode: "numeric"
-//       }}
-//       InputLabelProps={{
-//         classes: {
-//           root: classes.cssLabel,
-         
-//         },
-//       }}
-//       /><br/>
-//       <TextField className={classes.email} id="outlined-basic" label="Email" variant="outlined" 
-//        InputProps={{
-//         classes: {
-//           root: classes.cssOutlinedInput,
-//           notchedOutline: classes.notchedOutline,
-//         },
-//         inputMode: "numeric"
-//       }}
-//       InputLabelProps={{
-//         classes: {
-//           root: classes.cssLabel,
-         
-//         },
-//       }}
-//       /><br/>
-//       <TextField
-//       className={classes.area}
-//         id="outlined-multiline-static"
-//         label="Message"
-//         multiline
-//         rows={10}
-//         variant="outlined"
-//         InputProps={{
-//           classes: {
-//             root: classes.cssOutlinedInput,
-
-//             notchedOutline: classes.notchedOutline,
-//           },
-//           inputMode: "numeric"
-//         }}
-//         InputLabelProps={{
-//           classes: {
-//             root: classes.cssLabel,
-           
-//           },
-//         }}
-       
-//       /><br/>
-//        <Button
-//         variant="contained"
-//         color="primary"
-//         className={classes.button}
-//         endIcon={<SendIcon/>}
-//       >
-//         Send
-//       </Button>
-//     </form>
-//     </section>
-//   )
-// }
-
-//export default Contact
