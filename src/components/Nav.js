263d42bab257a66bb3f3120d2c8a6de26f6e62a9
serge aristide <nsergearistide@gmail.com>
2020-12-04 19:06:46 -0500

    added:responsive  burger menu on nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,29 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { CSSTransition } from "react-transition-group";
 
 function Nav() {
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-// const navStyle = {
-//   color:'lightgray'
-// };
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = mediaQuery => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
 
   return (
     <nav>
       <h3>Golden-Face</h3>
+
+      <CSSTransition
+           in={!isSmallScreen || isNavVisible}
+           timeout={350}
+           classNames="NavAnimation"
+           unmountOnExit
+          >
+
       <ul className='nav-links'>
       <Link to="/Home">
-        <li>Home</li>
+        <li onClick = {toggleNav}>Home</li>
       </Link>
       <Link  to="/About">
-        <li>About</li>
+        <li onClick = {toggleNav}>About</li>
       </Link>
       <Link  to="/Services">
-        <li>Services</li>
+        <li onClick = {toggleNav}>Services</li>
       </Link>
       <Link  to="/Contact">
-        <li>Contact</li>
+        <li onClick = {toggleNav}>Contact</li>
       </Link>
       </ul>
+      </CSSTransition>
+      <a  className = "burger-icon" onClick = {toggleNav}>
+      <img src="/iconfinder_Burger_Menu.png" width="24" height="24" alt="Phone"/>
+      </a>
     </nav>
   )
 }
