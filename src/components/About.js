989cd14eb2957d68930d490400dd9712953512e2
serge aristide <nsergearistide@gmail.com>
2020-12-07 16:07:56 -0500

    fixed: about page paragraph text

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,14 @@ import React from "react";
 function About() {
   return (
     <section className="home">
-      <div className = "about-pic">
-        
-      </div>
+      <div className = "about-pic"></div>
       <h1>About Golden-Face</h1>
       <hr></hr>
       <p>
       ‘Organization Name’ is a family-possessed business committed to giving perfection in our administration!
-       Our committed group of beauticians, nails and skin specialists accompany innumerable long periods of involvement in the magnificence business. We endeavor to make each customer our need, ensuring we tune in to everything about that we feature every individual’s common magnificence.
+       Our committed group of beauticians, nails and skin specialists accompany innumerable long periods of 
+       involvement in the magnificence business. We endeavor to make each customer our need, ensuring we 
+       tune in to everything about that we feature every individual’s common magnificence.
       </p>
       
     </section>
